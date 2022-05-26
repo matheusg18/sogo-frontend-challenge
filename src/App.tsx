7f210d1472e881f131a0cff7751832ad55d266b3
Matheus Santos <matheusgabriel.18@gmail.com>
2022-05-26 16:51:22 -0300

    feat: add "/register/person" route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
+import RegisterPerson from './pages/RegisterPerson/index.';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="/register">
+        <Route path="person" element={<RegisterPerson />} />
+      </Route>
     </Routes>
   );
 }
