4e26828b8176f3e4c3fe6cb5746ced3bca37a490
Matheus Santos <matheusgabriel.18@gmail.com>
2022-05-26 19:01:11 -0300

    refactor: changes formik to use its hook

--- src/pages/RegisterPerson/index..tsx
+++ src/pages/RegisterPerson/index..tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Field, Form, Formik } from 'formik';
+import { useFormik } from 'formik';
 
 function RegisterPerson() {
   const initialValues = {
@@ -16,31 +16,97 @@ function RegisterPerson() {
     complement: '',
   };
 
+  const formik = useFormik({ initialValues, onSubmit: console.log });
+
   return (
     <div>
       <h1>Cadastro de pessoa prestadora de serviço</h1>
-      <Formik initialValues={initialValues} onSubmit={console.log}>
-        {() => (
-          <Form>
-            <h2>Dados pessoais</h2>
-            <Field name="firstName" placeholder="Nome" />
-            <Field name="lastName" placeholder="Sobrenome" />
-            <Field name="cpf" placeholder="CPF" />
-            <Field name="birthDate" placeholder="Data de nascimento" />
-
-            <h2>Endereço</h2>
-            <Field name="cep" placeholder="CEP" />
-            <Field name="street" placeholder="Rua" />
-            <Field name="number" placeholder="número" />
-            <Field name="state" placeholder="Estado" />
-            <Field name="city" placeholder="Cidade" />
-            <Field name="district" placeholder="Bairro" />
-            <Field name="complement" placeholder="Complemento" />
-
-            <button type="submit">Cadastrar</button>
-          </Form>
-        )}
-      </Formik>
+      <form onSubmit={formik.handleSubmit}>
+        <h2>Dados pessoais</h2>
+        <input
+          type="text"
+          name="firstName"
+          placeholder="Nome"
+          onChange={formik.handleChange}
+          value={formik.values.firstName}
+        />
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Sobrenome"
+          onChange={formik.handleChange}
+          value={formik.values.lastName}
+        />
+        <input
+          type="text"
+          name="cpf"
+          placeholder="CPF"
+          onChange={formik.handleChange}
+          value={formik.values.cpf}
+        />
+        <input
+          type="text"
+          name="birthDate"
+          placeholder="Data de nascimento"
+          onChange={formik.handleChange}
+          value={formik.values.birthDate}
+        />
+        <h2>Endereço</h2>
+        <input
+          type="text"
+          name="cep"
+          placeholder="CEP"
+          onChange={formik.handleChange}
+          value={formik.values.cep}
+        />
+        <input
+          type="text"
+          name="street"
+          placeholder="Rua"
+          onChange={formik.handleChange}
+          value={formik.values.street}
+          readOnly
+        />
+        <input
+          type="text"
+          name="number"
+          placeholder="número"
+          onChange={formik.handleChange}
+          value={formik.values.number}
+        />
+        <input
+          type="text"
+          name="state"
+          placeholder="Estado"
+          onChange={formik.handleChange}
+          value={formik.values.state}
+          readOnly
+        />
+        <input
+          type="text"
+          name="city"
+          placeholder="Cidade"
+          onChange={formik.handleChange}
+          value={formik.values.city}
+          readOnly
+        />
+        <input
+          type="text"
+          name="district"
+          placeholder="Bairro"
+          onChange={formik.handleChange}
+          value={formik.values.district}
+          readOnly
+        />
+        <input
+          type="text"
+          name="complement"
+          placeholder="Complemento"
+          onChange={formik.handleChange}
+          value={formik.values.complement}
+        />
+        <button type="submit">Cadastrar</button>
+      </form>
     </div>
   );
 }
