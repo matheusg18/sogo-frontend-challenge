d51436800a201dc2165d94b31a0137c7f587d3ad
Matheus Santos <matheusgabriel.18@gmail.com>
2022-05-26 16:50:56 -0300

    feat: create RegisterPerson page

---
+++ src/pages/RegisterPerson/index..tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RegisterPerson() {
+  return (
+    <div>
+      <h1>Cadastro de pessoa prestadora de serviço</h1>
+    </div>
+  );
+}
+
+export default RegisterPerson;
