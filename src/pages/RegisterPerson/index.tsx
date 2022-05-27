646f132b0057e2f1340424933a577cb66fc5cf5a
Matheus Santos <matheusgabriel.18@gmail.com>
2022-05-27 15:08:16 -0300

    feat: add styles to RegisterPerson

--- src/pages/RegisterPerson/index.tsx
+++ src/pages/RegisterPerson/index.tsx
@@ -5,6 +5,7 @@ import { IRegisterPersonFormValues } from '../../interfaces';
 import TextInput from '../../components/TextInput';
 import { registerPersonValidation } from '../../validations';
 import Button from '../../components/Button';
+import './styles.scss';
 
 function RegisterPerson() {
   const initialValues: IRegisterPersonFormValues = {
@@ -40,29 +41,31 @@ function RegisterPerson() {
   };
 
   return (
-    <div>
-      <h1>Cadastro de pessoa prestadora de serviço</h1>
-      <form onSubmit={formik.handleSubmit}>
-        <h2>Dados pessoais</h2>
-
-        <TextInput name="firstName" placeholder="Nome" formik={formik} />
-        <TextInput name="lastName" placeholder="Sobrenome" formik={formik} />
-        <TextInput name="cpf" placeholder="CPF" formik={formik} />
-        <TextInput name="birthDate" placeholder="Data de nascimento" formik={formik} />
-
-        <h2>Endereço</h2>
-
-        <TextInput name="cep" placeholder="CEP" handleBlur={handleCepBlur} formik={formik} />
-        <TextInput name="street" placeholder="Rua" formik={formik} readOnly />
-        <TextInput name="number" placeholder="Número" formik={formik} />
-        <TextInput name="state" placeholder="Estado" formik={formik} readOnly />
-        <TextInput name="city" placeholder="Cidade" formik={formik} readOnly />
-        <TextInput name="district" placeholder="Bairro" formik={formik} readOnly />
-        <TextInput name="complement" placeholder="Complemento" formik={formik} />
-
-        <Button type="submit">Cadastrar</Button>
-      </form>
-    </div>
+    <main className="register-person-page">
+      <div className="register-person-page__container">
+        <h1 className="register-person-page__h1">Cadastro de pessoa prestadora de serviço</h1>
+        <form className="register-person-page__form" onSubmit={formik.handleSubmit}>
+          <h2 className="register-person-page__h2">Dados pessoais</h2>
+          <div className="register-person-page__personal-data-container">
+            <TextInput name="firstName" placeholder="Nome" formik={formik} />
+            <TextInput name="lastName" placeholder="Sobrenome" formik={formik} />
+            <TextInput name="cpf" placeholder="CPF" formik={formik} />
+            <TextInput name="birthDate" placeholder="Data de nascimento" formik={formik} />
+          </div>
+          <h2 className="register-person-page__h2">Endereço</h2>
+          <div className="register-person-page__address-data-container">
+            <TextInput name="cep" placeholder="CEP" handleBlur={handleCepBlur} formik={formik} />
+            <TextInput name="street" placeholder="Rua" formik={formik} readOnly />
+            <TextInput name="number" placeholder="Número" formik={formik} />
+            <TextInput name="state" placeholder="Estado" formik={formik} readOnly />
+            <TextInput name="city" placeholder="Cidade" formik={formik} readOnly />
+            <TextInput name="district" placeholder="Bairro" formik={formik} readOnly />
+            <TextInput name="complement" placeholder="Complemento" formik={formik} />
+          </div>
+          <Button type="submit">Cadastrar</Button>
+        </form>
+      </div>
+    </main>
   );
 }
 
