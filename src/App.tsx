31b06486c5cf4e562983856fb4fa04d70add6a75
Matheus Santos <matheusgabriel.18@gmail.com>
2022-05-27 00:33:31 -0300

    fix: wrong file extension

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
-import RegisterPerson from './pages/RegisterPerson/index.';
+import RegisterPerson from './pages/RegisterPerson';
 
 function App() {
   return (
