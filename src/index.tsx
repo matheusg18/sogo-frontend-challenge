d31e3949b9f3a3ebddeb3e33308357c0195586ce
Matheus Santos <matheusgabriel.18@gmail.com>
2022-05-29 20:52:53 -0300

    refactor: change sass folder name

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './styles/reset.scss';
+import './sass/reset.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
