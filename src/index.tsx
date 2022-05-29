c3e852673f08292edb2ba9b13c258e861fac2e94
Matheus Santos <matheusgabriel.18@gmail.com>
2022-05-29 07:06:37 -0300

    feat: add seed to window

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import * as utils from './utils';
 import './styles/reset.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+// @ts-ignore
+window.seed = utils.seed;
 
 root.render(
   <React.StrictMode>
