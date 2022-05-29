6e44a85f7da03952939b855a15c8a2e7fff81daa
Matheus Santos <matheusgabriel.18@gmail.com>
2022-05-28 23:32:33 -0300

    feat: adjust button to work with filter selection

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,18 +1,21 @@
-import React, { MouseEvent } from 'react';
+import React from 'react';
 import './styles.scss';
 
 type PropTypes = {
   type?: 'button' | 'submit' | 'reset';
   variant?: 'primary' | 'filter' | 'small-filter';
-  handleClick?: (event?: MouseEvent<HTMLButtonElement>) => void;
+  state?: 'normal' | 'selected';
+  value?: string;
+  handleClick?: any;
   children: string;
 };
 
-function Button({ type = 'button', variant = 'primary', handleClick, children }: PropTypes) {
+function Button({ type = 'button', variant = 'primary', state = 'normal', value, handleClick, children }: PropTypes) {
   return (
     <button
-      className={`button-component button-component--${variant}`}
+      className={`button-component button-component--${variant} button-component--${state}`}
       type={type}
+      value={value}
       onClick={handleClick}
     >
       {children}
