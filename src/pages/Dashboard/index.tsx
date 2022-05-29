d647563b3bf76156b81e4e340702f42baa1bb23e
Matheus Santos <matheusgabriel.18@gmail.com>
2022-05-28 23:33:08 -0300

    feat: add contracts to expire logic

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -6,14 +6,27 @@ import DashboardCard from '../../components/DashboardCard';
 import Button from '../../components/Button';
 import './styles.scss';
 
+type filterType = 'today' | 'next week' | 'next month' | 'next 2 weeks' | 'future';
+
 function Dashboard() {
   const [contracts, setContracts] = useState(0);
   const [averageContractTime, setAverageContractTime] = useState({ value: '0', unity: 'dias' });
+  const [contractsToExpire, setContractsToExpire] = useState(0);
+  const [expireInterval, setExpireInterval] = useState<filterType>('future');
 
   useEffect(() => {
     setContracts(services.getSavedContracts().length);
     setAverageContractTime(services.getAverageContractTime());
+    setContractsToExpire(services.filterContracts(expireInterval).length);
   }, []);
+
+  useEffect(() => {
+    setContractsToExpire(services.filterContracts(expireInterval).length);
+  }, [expireInterval]);
+
+  const handleFilterButtonClick = (filter: filterType): void => {
+    setExpireInterval((prev) => (prev === filter ? 'future' : filter));
+  };
 
   return (
     <main className="dashboard-page">
@@ -30,13 +43,41 @@ function Dashboard() {
         <DashboardCard
           header="Contratos a vencer"
           icon={<FaCalendarTimes size="52px" />}
-          displayData={10}
+          displayData={contractsToExpire}
         >
           <div className="dashboard-page__filter-wrapper">
-            <Button variant="small-filter">30 dias</Button>
-            <Button variant="small-filter">15 dias</Button>
-            <Button variant="small-filter">7 dias</Button>
-            <Button variant="small-filter">Hoje</Button>
+            <Button
+              variant="small-filter"
+              value="next month"
+              state={expireInterval === 'next month' ? 'selected' : 'normal'}
+              handleClick={() => handleFilterButtonClick('next month')}
+            >
+              30 dias
+            </Button>
+            <Button
+              variant="small-filter"
+              value="next 2 weeks"
+              state={expireInterval === 'next 2 weeks' ? 'selected' : 'normal'}
+              handleClick={() => handleFilterButtonClick('next 2 weeks')}
+            >
+              15 dias
+            </Button>
+            <Button
+              variant="small-filter"
+              value="next week"
+              state={expireInterval === 'next week' ? 'selected' : 'normal'}
+              handleClick={() => handleFilterButtonClick('next week')}
+            >
+              7 dias
+            </Button>
+            <Button
+              variant="small-filter"
+              value="today"
+              state={expireInterval === 'today' ? 'selected' : 'normal'}
+              handleClick={() => handleFilterButtonClick('today')}
+            >
+              Hoje
+            </Button>
           </div>
         </DashboardCard>
         <DashboardCard
