e550fe26a353fa081f15e9da9b08056121f083e2
Matheus Santos <matheusgabriel.18@gmail.com>
2022-05-30 22:58:28 -0300

    feat: add cpf integrity validation

--- src/validations/cpfSchema.ts
+++ src/validations/cpfSchema.ts
@@ -2,7 +2,7 @@ import * as yup from 'yup';
 
 const cpfSchema = yup
   .string()
-  .matches(/^\d{11}$/, 'CPF inválido')
+  .matches(/^\d{11}$/, 'Formato inválido')
   .required('CPF é obrigatório');
 
 export default cpfSchema;
