bfc0e1db8859258274691c22ca66c25645686df4
Matheus Santos <matheusgabriel.18@gmail.com>
2022-05-31 21:00:05 -0300

    test: fix RegisterContract broken test

--- src/pages/RegisterContract/test.tsx
+++ src/pages/RegisterContract/test.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import { act, render, screen } from '@testing-library/react';
+import { act, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import moment from 'moment';
 import RegisterContract from './index';
 import * as services from '../../services';
 import { genContractNumber } from '../../utils/genContractNumber';
+import { renderWithReactAlert } from '../../testUtils';
 
 jest.mock('../../utils/genContractNumber');
 
 describe('RegisterContract - unit test', () => {
+  beforeAll(() => {
+    // seed cpf
+    localStorage.setItem('@sogo/savedPersons', JSON.stringify([{ cpf: '14516861020' }]));
+  });
+
   afterAll(() => {
     jest.restoreAllMocks();
   });
@@ -16,7 +22,7 @@ describe('RegisterContract - unit test', () => {
   it('should register a contract', async () => {
     (genContractNumber as jest.Mock).mockReturnValue('0123456789');
 
-    render(<RegisterContract />);
+    renderWithReactAlert(<RegisterContract />);
 
     const registrationDateInput = screen.getByPlaceholderText('Data de registro');
     const dueDateInput = screen.getByPlaceholderText('Data de vencimento');
